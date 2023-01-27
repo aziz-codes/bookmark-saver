82869b1f7d8b8cf591e7437345889b32dc581c89
aziz-codes <azizisoffline@gmail.com>
2023-01-28 00:12:18 +0500

    setting input for adding tabs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
-
+import { BookOpenIcon, TrashIcon } from "@heroicons/react/24/outline";
 const Main = () => {
   const [tabs, setTabs] = useState([]);
   return (
     <div
-      className="text-red-500 min-w-[400px] flex
+      className="min-w-[400px] flex
    flex-col gap-2"
     >
-      <div className="p-1">Hello</div>
+      <div className="flex flex-row items-center mt-5 border m-5 rounded-md">
+        <input
+          type="text"
+          className="outline-none  rounded-md p-1 w-96 mr-4 ml-4 "
+          placeholder="Enter custom address"
+        />
+        <BookOpenIcon className="h-6 w-6 cursor-pointer" title="Save" />
+      </div>
+      <div className="p-1 flex flex-row gap-4 mx-auto min-h-[600px] overflow-y-auto"></div>
     </div>
   );
 };
